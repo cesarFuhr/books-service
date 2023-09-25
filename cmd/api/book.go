f3310e937671830b3e51063d8654c8ec9e78051d
Ramon Reichert <ramonreichert.rr@gmail.com>
2023-09-25 09:15:47 -0300

    wip defining layers

---
+++ cmd/api/book.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Book struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Price     *float32  `json:"price"`
+	Inventory *int      `json:"inventory"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+	Archived  bool      `json:"archived"`
+}
+
+/* Verifies if all entry fields are filled and returns a warning message if so. */
+func filledFields(bookEntry Book) error {
+	if bookEntry.Name == "" {
+		return errResponseBookEntryBlankFileds
+	}
+	if bookEntry.Price == nil {
+		return errResponseBookEntryBlankFileds
+	}
+	if bookEntry.Inventory == nil {
+		return errResponseBookEntryBlankFileds
+	}
+
+	return nil
+}
