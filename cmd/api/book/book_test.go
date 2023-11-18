a91d0ae69220580711ec9fb005e95a78b433e2e6
Ramon Reichert <ramonreichert.rr@gmail.com>
2023-11-18 15:14:47 -0300

    wip service test with mock Repository

--- cmd/api/book/book_test.go
+++ cmd/api/book/book_test.go
@@ -3,15 +3,15 @@ package book_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/books-service/cmd/api/book"
 	"github.com/google/uuid"
 	"github.com/matryer/is"
+	gomock "go.uber.org/mock/gomock"
 )
 
-var s *book.Service
-
-//var mS *MockRepositoryMockRecorder
+var mS *book.Service
 
 /*func TestMain(m *testing.M) {
 
@@ -21,13 +21,27 @@ var s *book.Service
 func TestCreateBook(t *testing.T) {
 	t.Run("creates a book without errors", func(t *testing.T) {
 		is := is.New(t)
+		ctrl := gomock.NewController(t)
+		mockRepo := NewMockRepository(ctrl)
+		mS = book.NewService(mockRepo)
+
 		reqBook := book.CreateBookRequest{
 			Name:      "Service tester book",
 			Price:     toPointer(float32(100.0)),
 			Inventory: toPointer(99),
 		}
 		fmt.Println(reqBook)
-		createdBook, err := s.CreateBook(reqBook)
+
+		mockRepo.EXPECT().CreateBook(book.Book{
+			ID:        uuid.New(), //Atribute an ID to the entry
+			Name:      "Service tester book",
+			Price:     toPointer(float32(100.0)),
+			Inventory: toPointer(99),
+			CreatedAt: time.Now().UTC().Round(time.Millisecond),
+			UpdatedAt: time.Now().UTC().Round(time.Millisecond),
+			Archived:  false,
+		})
+		createdBook, err := mS.CreateBook(reqBook)
 		is.NoErr(err)
 		is.True(createdBook.ID != uuid.Nil)
 		is.Equal(createdBook.Name, reqBook.Name)
@@ -41,7 +55,9 @@ func toPointer[T any](v T) *T {
 	return &v
 }
 
-/*func TestCreateBook(t *testing.T) {
+/*func TestCreateBookCOMMOCK(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockRepo := NewMockRepository(ctrl)
+	mS := book.NewService(mockRepo)
+
 }*/
