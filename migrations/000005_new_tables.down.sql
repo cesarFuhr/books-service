bfe3a9c983c8db504a6ef496c0da5a4c60083234
Ramon Reichert <ramonreichert.rr@gmail.com>
2024-02-20 09:16:53 -0300

    fixed new tables names and formatting

--- migrations/000005_new_tables.down.sql
+++ migrations/000005_new_tables.down.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS public.userstable;
+DROP TABLE IF EXISTS public.users;
 
-DROP TABLE IF EXISTS public.paymentstable;
+DROP TABLE IF EXISTS public.payments;
 
-DROP TABLE IF EXISTS public.books_orderstable;
+DROP TABLE IF EXISTS public.books_orders;
 
-DROP TABLE IF EXISTS public.orderstable;
+DROP TABLE IF EXISTS public.orders;
 
 DROP TYPE IF EXISTS access;
 
--- migrations/000005_new_tables.up.sql
+++ migrations/000005_new_tables.up.sql
@@ -1,37 +1,39 @@
 CREATE TYPE access AS ENUM ('user', 'admin');
 
-CREATE TABLE IF NOT EXISTS public.userstable
+CREATE TABLE IF NOT EXISTS public.users
 (
-    user_id uuid PRIMARY KEY NOT NULL,
-    name text   NOT NULL,
-    user_access access DEFAULT 'user'
+user_id uuid PRIMARY KEY NOT NULL,
+name text NOT NULL,
+user_role access DEFAULT 'user'
 );
 
 CREATE TYPE order_status AS ENUM ('accepting_items', 'canceled', 'waiting_payment', 'paid');
 
-CREATE TABLE IF NOT EXISTS public.orderstable
+CREATE TABLE IF NOT EXISTS public.orders
 (
-order_id		uuid PRIMARY KEY NOT NULL,
-purchaser_id 	uuid,	
-order_status	order_status DEFAULT 'accepting_items',
-created_at 		timestamp with time zone DEFAULT now(),
-updated_at 	timestamp with time zone DEFAULT now()
+order_id uuid PRIMARY KEY NOT NULL,
+purchaser_id uuid,	
+order_status order_status DEFAULT 'accepting_items',
+created_at timestamp with time zone DEFAULT now(),
+updated_at timestamp with time zone DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS public.books_orderstable (
-    order_id		uuid REFERENCES public.orderstable ON DELETE CASCADE,
-    book_id		uuid REFERENCES public.bookstable ON DELETE CASCADE,
-    book_units integer,
-    book_price_at_order	numeric(6,2),
-    created_at 		timestamp with time zone DEFAULT now(),
-    updated_at 	timestamp with time zone DEFAULT now(),
-    PRIMARY KEY (order_id, book_id)
+CREATE TABLE IF NOT EXISTS public.books_orders
+(
+order_id uuid REFERENCES public.orderstable ON DELETE CASCADE,
+book_id	uuid REFERENCES public.bookstable ON DELETE CASCADE,
+book_units integer,
+book_price_at_order	numeric(6,2),
+created_at timestamp with time zone DEFAULT now(),
+updated_at timestamp with time zone DEFAULT now(),
+PRIMARY KEY (order_id, book_id)
 );
 
-CREATE TABLE IF NOT EXISTS public.paymentstable (
-    order_id		uuid REFERENCES public.orderstable ON DELETE CASCADE,
-   order_paid          BOOLEAN DEFAULT false,
-    created_at 		timestamp with time zone DEFAULT now(),
-    updated_at 	timestamp with time zone DEFAULT now(),
-    PRIMARY KEY (order_id)
+CREATE TABLE IF NOT EXISTS public.payments
+(
+order_id uuid REFERENCES public.orderstable ON DELETE CASCADE,
+order_paid BOOLEAN DEFAULT false,
+created_at timestamp with time zone DEFAULT now(),
+updated_at timestamp with time zone DEFAULT now(),
+PRIMARY KEY (order_id)
 );
